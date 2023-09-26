dd218b880991b27d862b8a6872c97378c0a4f7cd
CaioCPS <caio.cesar@ducash.com.br>
2023-09-25 23:11:19 -0300

    fix: fix import of default theme on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { Button } from './components/Button';
-import { defaultTheam } from './styles/theme/default';
 import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/theme/default';
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheam}>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button />
