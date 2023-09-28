9f212b54089075fde48e67bdd0177a518c3ebc3e
CaioCPS <caio.cesar@ducash.com.br>
2023-09-28 00:06:59 -0300

    refactor: :recycle: rename Header component to index.tsx

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from "./styles";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>Logo</span>
+      <nav>
+        <a href="">Timer</a>
+        <a href="">Historico</a>
+      </nav>
+    </HeaderContainer>
+  );
+}
