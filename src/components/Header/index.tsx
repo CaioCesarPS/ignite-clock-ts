17ce66bc6078499c9340e523bdbccab4e35ccd04
CaioCPS <caio.cesar@ducash.com.br>
2023-09-28 22:44:31 -0300

    feat: add style to headerContainer and implementing react-router-dom navegation into header comp

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
-import { HeaderContainer } from "./styles";
+import { HeaderContainer } from './styles';
+import logo from '../../assets/logo.svg';
+import { Scroll, Timer } from '@phosphor-icons/react';
+import { NavLink } from 'react-router-dom';
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img src={logo} alt="" />
       <nav>
-        <a href="">Timer</a>
-        <a href="">Historico</a>
+        <NavLink to="/" title='Timer'>
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title='History'>
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   );
